a9f451805b3c4383e6650150767c05120ffd365d
Fatemeh Heidari <gabrina101@gmail.com>
2025-08-04 00:46:13 +0330

    working on filteration

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,16 +1,17 @@
 import Styles from "./ProductCard.module.css";
 import { RiMoreFill, RiShoppingCartLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { shortenText } from "../helpers/shortenText";
 
 function ProductCard({ product }) {
   const { id, title, price, image } = product;
   return (
     <div className={Styles.card}>
       <img src={image} />
-      <h1>{title}</h1>
+      <h1>{shortenText(title)}</h1>
       <span>{price} $</span>
       <div className={Styles.icons}>
-        <Link to={`/product/${id}`}>
+        <Link to={`/products/${id}`}>
           <RiMoreFill />
         </Link>
         <RiShoppingCartLine />
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -2,18 +2,91 @@ import { useProducts } from "../Contexts/ProductsContext";
 import ProductCard from "../Components/ProductCard";
 import Loader from "../Components/Loader";
 import Styles from "./ProductPage.module.css";
+import { MdSearch } from "react-icons/md";
+import { useEffect, useState } from "react";
+import { FaListUl } from "react-icons/fa";
+
 function ProductPage() {
+  useEffect(() => {
+    setFilteredProducts([...products]);
+  }, []);
   const products = useProducts();
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const searchHandler = () => {
+    setFilteredProducts(
+      products.filter((product) =>
+        product.title.toLowerCase().trim().includes(search.toLowerCase().trim())
+      )
+    );
+  };
+
+  const categoryHandler = (event) => {
+    const { tagName, innerText } = event.target;
+    if (tagName !== "LI") return;
+    setCategory(innerText.toLowerCase());
+  };
   return (
-    <div className={Styles.container}>
-      <div className={Styles.products}>
-        {!products.length && <Loader /> }
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+    <>
+      <div className={Styles.searchBar}>
+        <input
+          type="text"
+          placeholder="Type to search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <MdSearch />
+        </button>
       </div>
-      <div className={Styles.sidebar}>Sidebar</div>
-    </div>
+      <div className={Styles.container}>
+        <div className={Styles.products}>
+          {!products.length && <Loader />}
+          {filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+        <div className={Styles.sidebar}>
+          <header>
+            <FaListUl color="var(--blue)" />
+            <span>Categories</span>
+          </header>
+          <ul onClick={categoryHandler}>
+            <li className={category === "All" ? Styles.selectedCategory : ""}>
+              All
+            </li>
+            <li
+              className={
+                category === "Electronics" ? Styles.selectedCategory : ""
+              }
+            >
+              Electronics
+            </li>
+            <li
+              className={category === "Jewelery" ? Styles.selectedCategory : ""}
+            >
+              Jewelery
+            </li>
+            <li
+              className={
+                category === "Men's clothing" ? Styles.selectedCategory : ""
+              }
+            >
+              Men's clothing
+            </li>
+            <li
+              className={
+                category === "Women's clothing" ? Styles.selectedCategory : ""
+              }
+            >
+              Women's clothing
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
